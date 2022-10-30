0e2791de0d558da629aab7dd35a4acf591ca4ae1
smtnhacker <themikosurara@gmail.com>
2022-10-30 22:59:12 +0800

    chore: add client production endpoints

---
+++ client/public/endpoints.development.js
@@ -0,0 +1,2 @@
+const WEBSERVER_PATH = 'http://localhost:8080'
+const API_ENDPOINT = 'http://localhost:5000/api/'
---
+++ client/public/endpoints.js
@@ -0,0 +1,2 @@
+const WEBSERVER_PATH = 'https://anti-semantle-webserver.onrender.com/'
+const API_ENDPOINT = 'https://anti-semantle-api.onrender.com/'
